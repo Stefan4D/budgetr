5ce58bb9cf57e1ef0b7f4f8a86cc078bef74e58a
Stefan <blockchain.crypto.stefan@gmail.com>
2023-09-26 08:30:57 +0100

    Add Tailwind, PostCSS build pipeline to Vite. Add Tailwind autosort plugin to Prettier.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <h1 className="mb-5 ml-5 mt-10 px-10 py-10 text-4xl text-black">
+      Hello World
+    </h1>
+  );
 }
