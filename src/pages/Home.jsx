45ead08676d01562eadd878b8b068968c0d4f691
Stefan <blockchain.crypto.stefan@gmail.com>
2023-09-26 19:40:31 +0100

    Updated Hello World

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Home() {
   return (
     <h1 className="mb-5 ml-5 mt-10 px-10 py-10 text-4xl text-black">
-      Hello World
+      Hello World!
     </h1>
   );
 }
