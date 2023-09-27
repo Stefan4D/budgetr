821f147d572103dac88bb7bf079534c6419c0942
AshivaA <136107176+AshivaA@users.noreply.github.com>
2023-09-27 18:57:36 +0100

    update navbar

--- src/components/TopNavBar.jsx
+++ src/components/TopNavBar.jsx
@@ -4,13 +4,15 @@ export default function TopNavBar() {
   return (
     // <div className="mb-3">TopNavBar</div>
     <div>
-      <nav className="bg-emerald-800 shadow-lg" >
+      <nav className="bg-emerald-800 shadow-lg rounded-lg" >
         <div className="container max-auto" >
           <div className="sm:flex" >
-            <a href="#" className='text-white text-2xl font-semibold p-3 justify-left  hover:text-lg' >BUDGETR</a>
-            <ul className='text-white sm:self-center text-XL border-t sm:border-none '>
-              <li className='sm:inline-block'>
-                <a href="#" className='p-3 hover:text-blue  hover:text-cyan-400'>Home</a>
+            {/* <a href="#" className='text-white text-2xl font-semibold p-3 justify-left  hover:text-lg' >BUDGETR</a> */}
+            <button className='bg-lime-700 rounded-lg text-white text-2xl font-semibold p-3 justify-left' >BUDGETR</button>
+
+            <ul className='text-white sm:self-center text-XL border-t sm:border-none'>
+              <li className='sm:inline-block justify-right'>
+                <a href="#" className='p-3 hover:text-blue  hover:text-cyan-400 '>Home</a>
               </li>
               <li className='sm:inline-block'>
                 <a href="#" className='p-3 hover:text-blue  hover:text-cyan-400'>Add Expense</a>
