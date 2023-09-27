4d0b9a58c5fd7ad1c62e9a77c59a6b303be6c0a3
AshivaA <136107176+AshivaA@users.noreply.github.com>
2023-09-27 16:41:16 +0100

    added basic-navbar

--- src/components/TopNavBar.jsx
+++ src/components/TopNavBar.jsx
@@ -1,5 +1,46 @@
 import React from 'react';
 
 export default function TopNavBar() {
-  return <div className="mb-3">TopNavBar</div>;
+  return (
+    // <div className="mb-3">TopNavBar</div>
+    <div>
+      <nav className="bg-emerald-800 shadow-lg" >
+        <div className="container max-auto" >
+          <div className="sm:flex" >
+            <a href="#" className='text-white text-2xl font-semibold p-3 justify-left  hover:text-lg' >BUDGETR</a>
+            <ul className='text-white sm:self-center text-XL border-t sm:border-none '>
+              <li className='sm:inline-block'>
+                <a href="#" className='p-3 hover:text-blue  hover:text-cyan-400'>Home</a>
+              </li>
+              <li className='sm:inline-block'>
+                <a href="#" className='p-3 hover:text-blue  hover:text-cyan-400'>Add Expense</a>
+              </li>
+
+              <li className='sm:inline-block'>
+                <a href="#" className='p-3 hover:text-blue  hover:text-cyan-400 '>Dashboard</a>
+              </li>
+
+              <li className='sm:inline-block'>
+                <a href="#" className='p-3 hover:text-cyan-400'>Modify Expense</a>
+              </li>
+
+              <li className='sm:inline-block'>
+                <a href="#" className='p-3 hover:text-blue  hover:text-cyan-400 '>View Expenses</a>
+              </li>
+             
+
+
+            </ul>
+
+            
+
+          </div>
+
+        </div>
+      </nav>
+    </div>
+  )
+  
+  
+  
 }
