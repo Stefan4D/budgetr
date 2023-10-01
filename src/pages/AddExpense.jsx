42a0b4f9a00ad730c70372a13ff3ccebd89bd61e
Stefan <blockchain.crypto.stefan@gmail.com>
2023-10-01 16:51:47 +0100

    Fix Form/form case issue in filename

--- src/pages/AddExpense.jsx
+++ src/pages/AddExpense.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Form from '../components/AddExpense/Form';
+import Form from '../components/AddExpense/form';
 
 export default function AddExpense() {
   return (
