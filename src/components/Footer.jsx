c2ccd813367779a46b87b2cc635f09b726e356b2
noeamelya <noeamelyaofficial@hotmail.com>
2023-10-02 20:17:45 +0100

    update footer and add link netlify

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,13 +8,17 @@ import {
 
 export default function Footer() {
   return (
-    <footer className="rounded-lg bg-slate-800 px-4 py-20 text-white lg:px-48  ">
+    <footer className="rounded-lg bg-slate-800 px-2 py-20 text-white lg:px-48  ">
       <div className="grid-col-1 container mx-auto mb-8 grid md:grid-cols-4 ">
         <div className="mb-4 px-2 md:mb-4">
-          <span className="py-2 text-[20px] font-semibold uppercase text-slate-400">
-            Budgetr
-          </span>
-          <p className="my-4 text-[15px]">Manage the budget plan</p>
+          <h1 className="justify-left font-brand p-3 text-3xl font-black text-white">
+            Budget<span className="text-cyan-400">r</span>
+          </h1>
+          <p className="mt-2 p-1 text-[15px]">
+            Manage the budget plan and level-up your spending, seize control of
+            your finances and take the wheel on your money with the number one
+            budgeting app from FinanceFlow.
+          </p>
         </div>
         <div className="mb-4 px-2 md:mb-4">
           <h2 className="py-2 text-[16px] font-semibold uppercase text-slate-400">
@@ -45,7 +49,10 @@ export default function Footer() {
           </a>
         </div>
         <div className="flex space-x-2">
-          <a href="/" className="text-slate-500">
+          <a
+            href="https://github.com/Stefan4D/budgetr"
+            className="text-slate-500"
+          >
             <AiFillGithub className="text-[30px]" />
           </a>
           <a href="/" className="text-slate-500">
@@ -62,7 +69,10 @@ export default function Footer() {
       <hr className="w-100 clearfix d-md-none" />
       <div className="justify-center p-3 text-center text-[10px]">
         © 2023 Copyright :{' '}
-        <a className="text-white" href="http://">
+        <a
+          className="text-white"
+          href="https://financeflow-budgetr.netlify.app/"
+        >
           Budgetr.com
         </a>
       </div>
