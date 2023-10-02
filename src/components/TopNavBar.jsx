7c3c56d5ce3bacce43f4188d7e1a1d5df9e4b3c8
Syed Mahbub <syedmahbub.mu@hotmail.com>
2023-10-02 20:56:52 +0100

    fix navigation

--- src/components/TopNavBar.jsx
+++ src/components/TopNavBar.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { FaSignInAlt } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 export default function TopNavBar() {
   return (
     <nav className="bg-slate-800 shadow-lg">
-      <div className="max-auto container">
-        <div className="sm:flex">
+      <div className="max-auto container-fluid">
+        <div className="flex">
           <Link
             to="/"
             className="justify-left p-3 font-brand text-3xl font-black text-white"
@@ -14,60 +13,17 @@ export default function TopNavBar() {
             Budget<span className="text-cyan-400">r</span>
           </Link>
 
-          <ul className="border-t text-xl text-white sm:self-center sm:border-none">
+          <Link
+            to="/app"
+            className="ml-auto self-center p-3 text-white hover:text-cyan-400"
+          >
+            Go to App
+          </Link>
+          {/* <ul className="border-t text-xl text-white sm:self-center sm:border-none">
             <li className="justify-right sm:inline-block">
-              <Link
-                to="/"
-                className="hover:text-blue p-3  hover:text-cyan-400 "
-              >
-                Home
-              </Link>
+              
             </li>
-
-            <li className="sm:inline-block">
-              <Link
-                to="/app/add"
-                className="hover:text-blue p-3  hover:text-cyan-400"
-              >
-                Add Expense
-              </Link>
-            </li>
-
-            <li className="sm:inline-block">
-              <Link
-                to="/app/dashboard"
-                className="hover:text-blue p-3  hover:text-cyan-400 "
-              >
-                Dashboard
-              </Link>
-            </li>
-
-            <li className="sm:inline-block">
-              <Link to="/app/modify" className="p-3 hover:text-cyan-400">
-                Modify Expense
-              </Link>
-            </li>
-
-            <li className="sm:inline-block">
-              <Link
-                to="/app/view"
-                className="hover:text-blue p-3 hover:text-cyan-400"
-              >
-                View Expenses
-              </Link>
-            </li>
-          </ul>
-          <ul className="border-t text-xl text-white sm:self-center sm:border-none">
-            <li className="flex items-center ">
-              <Link
-                to="/app"
-                className='className="hover:text-blue p-3 hover:text-cyan-400'
-              >
-                <FaSignInAlt className="mr-2" />
-                <span>Sign in</span>
-              </Link>
-            </li>
-          </ul>
+          </ul> */}
         </div>
       </div>
     </nav>
