1710c5708fdd937db239e24591de09c84efe30ed
Stefan <blockchain.crypto.stefan@gmail.com>
2023-10-02 21:08:35 +0100

    Fix View on mobile; Add dynamic charts

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,52 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import dayjs from 'dayjs';
 import isBetween from 'dayjs/plugin/isBetween';
+import currency from 'currency.js';
 
 import useLocalForage from '../hooks/useLocalForage';
 
 import GraphCard from '../components/Dashboard/GraphCard';
 import Transactions from '../components/Dashboard/Transactions';
 
-// used for testing
-// import expenses from '../../__tests__/mockExpensesData';
 import globals from '../data/globals';
 
 dayjs.extend(isBetween);
 
 export default function Dashboard() {
-  const [transactions, setTransactions] = useState();
+  const [transactions, setTransactions] = useState([]);
+  const [barChartData, setBarChartData] = useState({});
+  const [categoriesChartData, setCategoriesChartData] = useState({});
   const [value, setValue, pending] = useLocalForage(globals.db);
 
-  useEffect(() => {
-    console.log('useEffect: Getting localForage');
-    setTransactions(value); // set application state
-    console.table(value);
-
-    // const tx = transactions.filter((expense) => {
-    //   const date = dayjs(expense.date);
-    //   const startDate = dayjs('2022-01-01');
-    //   const endDate = dayjs('2022-12-31');
-    //   return date.isBetween(startDate, endDate);
-    // });
-    // console.log(tx.length);
-    // console.table(tx);
-  }, [value]); // get localForage on first render
-
-  // useEffect(() => {
-  //   console.log('useEffect: Updating localForage');
-  //   setValue(transactions); // set localForage state
-  // }, [transactions]); // update localForage when application state changes
-
-  // Sample graph data
-  const categoriesChartData = {
-    labels: ['Food', 'Beer'],
-    datasets: [
-      {
-        data: [100, 650],
-        backgroundColor: ['rgba(34, 211, 238, 1)', 'rgba(30, 41, 59, 0.5)'],
-      },
-    ],
-  };
   const categoriesChartOptions = {
     responsive: true,
     maintainAspectRatio: false,
@@ -54,8 +25,6 @@ export default function Dashboard() {
       position: 'bottom',
     },
   };
-
-  // BAR CHART DATA
 
   const barChartOptions = {
     responsive: true,
@@ -67,37 +36,108 @@ export default function Dashboard() {
     },
   };
 
-  const labels = ['January', 'February', 'March', 'April', 'May', 'June'];
+  const generateMonthlyData = (txs, year) => {
+    const data = Array(12).fill(0);
+    const labels = Array.from({ length: 12 }, (_, i) =>
+      dayjs(`${year}-${i + 1}-01`).format('MMMM')
+    );
 
-  const barChartData = {
-    labels,
-    datasets: [
-      {
-        label: 'GBP',
-        data: [60, 40, 80, 20, 60, 40],
-        backgroundColor: 'rgba(251, 146, 60, 1)',
-      },
-    ],
+    txs.forEach((tx) => {
+      const date = dayjs(tx.date);
+      if (date.year() === year) {
+        const monthIndex = date.month();
+        data[monthIndex] = currency(data[monthIndex]).add(
+          tx.convertedAmount
+        ).value;
+      }
+    });
+
+    return { labels, data };
   };
+
+  const generateCategoriesData = (txs) => {
+    // Extract unique categories from transactions
+    const categoriesSet = new Set(txs.map((tx) => tx.category));
+    const categories = [...categoriesSet];
+
+    const data = categories.map((cat) => {
+      return txs.reduce(
+        (acc, tx) =>
+          tx.category === cat
+            ? currency(acc).add(tx.convertedAmount).value
+            : acc,
+        0
+      );
+    });
+
+    return {
+      labels: categories,
+      data,
+    };
+  };
+
+  useEffect(() => {
+    if (value && value.length > 0) {
+      setTransactions(value);
+      const monthlyData = generateMonthlyData(value, 2023);
+      const catData = generateCategoriesData(value);
+
+      setBarChartData({
+        labels: monthlyData.labels,
+        datasets: [
+          {
+            label: 'GBP',
+            data: monthlyData.data,
+            backgroundColor: 'rgba(251, 146, 60, 1)',
+          },
+        ],
+      });
+
+      setCategoriesChartData({
+        labels: catData.labels,
+        datasets: [
+          {
+            data: catData.data,
+            // for the length of the data array, generate a random colour
+            backgroundColor: Array.from(
+              { length: catData.data.length },
+              (element, index) => {
+                const hue = (index * 30 + 100) % 360;
+                const saturation = index % 2 === 0 ? '50%' : '75%';
+                const lightness = '50%';
+                const alpha = 1;
+                return `hsla(${hue}, ${saturation}, ${lightness}, ${alpha})`;
+              }
+            ),
+            // backgroundColor: ['rgba(34, 211, 238, 1)', 'rgba(30, 41, 59, 0.5)'],
+          },
+        ],
+      });
+    }
+  }, [value]);
 
   return (
     <div className="min-h-[calc(100vh-100px)] w-full  flex-1 p-4 sm:min-h-[calc(100vh-76px)] md:w-1/2">
       <h2 className="text-xl font-bold">Dashboard</h2>
 
       <div className="mt-4 grid grid-cols-1 gap-4 sm:grid-cols-2">
-        <GraphCard
-          graphType="doughnut"
-          title="Categories"
-          data={categoriesChartData}
-          options={categoriesChartOptions}
-        />
+        {barChartData?.labels?.length > 0 && (
+          <GraphCard
+            graphType="bar"
+            title="Monthly Spend"
+            data={barChartData}
+            options={barChartOptions}
+          />
+        )}
 
-        <GraphCard
-          graphType="bar"
-          title="Monthly Spend"
-          data={barChartData}
-          options={barChartOptions}
-        />
+        {categoriesChartData?.labels?.length > 0 && (
+          <GraphCard
+            graphType="doughnut"
+            title="Categories"
+            data={categoriesChartData}
+            options={categoriesChartOptions}
+          />
+        )}
       </div>
 
       {pending && <p>Loading...</p>}
